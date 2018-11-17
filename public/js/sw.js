90a39f2162427ba5c7cc58896f85e98e1e001ad9
Omkar Pathak <omkarpathak27@gmail.com>
2018-11-17 09:50:00 +0530

    Added service workers for better page scores

---
+++ public/js/sw.js
@@ -0,0 +1,62 @@
+---
+layout: null
+---
+
+var urlsToCache = [];
+
+var CACHE_NAME = 'omkar-pathak-cache-v1';
+
+// Cache posts
+// Limits the number of posts that gets cached to 3
+// Reads a piece of front-matter in each post that directs the second loop to the folder where the assets are held
+{% for post in site.posts limit:3 %}
+  urlsToCache.push("{{ post.url }}")
+  {% for file in site.static_files %}
+    {% if file.path contains post.assets %}
+      urlsToCache.push("{{ file.path }}")
+    {% endif %}
+  {% endfor %}
+{% endfor %}
+
+// Cache pages
+// Do nothing if it's either an AMP page (as these are served via Googles cache) or the blog page
+// Fallback to the offline pages for these
+{% for page in site.html_pages %}
+  {% if page.path contains 'amp-html' or page.path contains 'blog' %}
+  {% else if %}
+    urlsToCache.push("{{ page.url }}")
+  {% endif %}
+{% endfor %}
+
+// Cache assets
+// Removed assets/posts because I only want assets from the most recent posts getting cached
+{% for file in site.static_files %}
+    {% if file.extname == '.js' or file.path contains '/assets/images' %}
+    urlsToCache.push("{{ file.path }}")
+    {% endif %}
+{% endfor %}
+
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+  });
