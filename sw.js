4007eec986b3cca8eee53c9bbec41b97d4105b4f
Omkar Pathak <omkarpathak27@gmail.com>
2020-05-07 12:39:03 +0530

    Reverted CSS and JS methods

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ layout: null
 
 var urlsToCache = [];
 
-var CACHE_NAME = 'omkar-pathak-cache-v9';
+var CACHE_NAME = 'omkar-pathak-cache-v10';
 
 // Cache posts
 // Limits the number of posts that gets cached to 3
