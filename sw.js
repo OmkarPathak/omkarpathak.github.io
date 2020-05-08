4acbd2673a9b0d6dfd19a96667277eb51d177501
Omkar Pathak <omkarpathak27@gmail.com>
2020-05-08 12:31:51 +0530

    Service worker activate bug fix

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ layout: null
 
 var urlsToCache = [];
 
-var CACHE_NAME = 'omkar-pathak-cache-v11';
+var CACHE_NAME = 'omkar-pathak-cache-v12';
 
 // Cache posts
 // Limits the number of posts that gets cached to 3
