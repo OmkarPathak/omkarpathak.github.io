0ac7befb7fbd4a5c63d24b9da9c22466f1fc49db
Omkar Pathak <omkarpathak27@gmail.com>
2020-05-08 12:29:03 +0530

    Service worker activate bug fix

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ layout: null
 
 var urlsToCache = [];
 
-var CACHE_NAME = 'omkar-pathak-cache-v10';
+var CACHE_NAME = 'omkar-pathak-cache-v11';
 
 // Cache posts
 // Limits the number of posts that gets cached to 3
@@ -52,10 +52,10 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.filter((cacheName) => {
-          return cacheName !== CACHE_NAME
-        }).map((cacheName) => {
-          return caches.delete(cacheName);
+        cacheNames.map((key) => {
+          if (key !== CACHE_NAME){
+            return caches.delete(key);
+          }
         })
       );
     })
