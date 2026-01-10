8b94650645a35942f486bcc73c30fd76388ffc06
Omkar Pathak <omkarpathak27@gmail.com>
2026-01-10 11:59:28 +0530

    modified service worker version

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ layout: null
 
 var urlsToCache = [];
 
-var CACHE_NAME = 'omkar-pathak-cache-v18';
+var CACHE_NAME = 'omkar-pathak-cache-v19';
 
 // Cache posts
 // Limits the number of posts that gets cached to 3
